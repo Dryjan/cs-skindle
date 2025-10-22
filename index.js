0d22db62375bedfe9a81bc23e6cd2a310cba0e95
Dryjan <145480200+Dryjan@users.noreply.github.com>
2025-10-22 18:47:32 +0200

    try to make smth

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ import { skinsArr } from './data/skinsArr.js';
 import { weaponsObj } from './data/weaponsObj.js';
 import { raritiesObj } from './data/raritiesObj.js';
 import { collectionsObj } from './data/collectionsObj.js';
+console.log(skinsArr, weaponsObj, raritiesObj, collectionsObj);
 
-console.log(collectionsObj, raritiesObj, skinsArr, weaponsObj);
+const form = document.getElementById('form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
+
+const inputGuess = document.getElementById('inputGuess');
+inputGuess.addEventListener('input', function() {
+  console.log();
+});
