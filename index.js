36c3fbebac4e0f08b7eea156f2804cf1f75c9c57
Dryjan <145480200+Dryjan@users.noreply.github.com>
2025-10-23 18:02:24 +0200

    add guess handler and css

--- index.js
+++ index.js
@@ -4,37 +4,55 @@ import { raritiesObj } from './data/raritiesObj.js';
 import { collectionsObj } from './data/collectionsObj.js';
 console.log(skinsArr, weaponsObj, raritiesObj, collectionsObj);
 
-const answer = skinsArr[Math.floor(Math.random() * 1358)];
-console.log(answer);
-let searchArr = [];
+function guess(skin) {
+  resultsArr = [];
+  divRes.innerHTML = '';
+  inputGuess.value = '';
+  divGue.innerHTML = '';
 
+  guessesArr.unshift(skin);
+  skinsArr.splice(skinsArr.indexOf(skin), 1);
+  console.log(guessesArr, skinsArr);
+
+  guessesArr.forEach(element => {
+    
+  });
+}
+
+const answer = skinsArr[Math.floor(Math.random() * skinsArr.length)];
+console.log(answer);
+const divRes = document.getElementById('results');
 const form = document.getElementById('form');
+const inputGuess = document.getElementById('inputGuess');
+const divGue = document.getElementById('guesses');
+let resultsArr = [];
+let guessesArr = [];
+
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 });
 
-const inputGuess = document.getElementById('inputGuess');
 inputGuess.addEventListener('input', function() {
   const inputVal = inputGuess.value.trim();
-  searchArr = [];
+
+  resultsArr = [];
+  divRes.innerHTML = '';
 
   if(inputVal) {
     skinsArr.forEach(element => {
       if(element.name.toLowerCase().includes(inputVal.toLowerCase())) {
-        searchArr.push(element);
+        resultsArr.push(element);
       }
     });
   }
-  console.log(inputVal, searchArr);
+  console.log(inputVal, resultsArr);
 
-  const divRes = document.getElementById('results');
-  divRes.innerHTML = '';
-  searchArr.forEach(element => {
+  resultsArr.forEach(element => {
     const result = document.createElement('div');
     result.className = 'result';
     result.style.backgroundColor = raritiesObj[element.rarity];
     result.innerText = element.name;
-    result.onclick = '';
+    result.onclick = function() { guess(element) };
     divRes.appendChild(result);
   });
 });
