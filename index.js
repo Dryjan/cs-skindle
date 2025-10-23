28c38677d02e02ce13a73c3e095ef74307fe4895
Dryjan <145480200+Dryjan@users.noreply.github.com>
2025-10-23 17:12:17 +0200

    add search display

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import { raritiesObj } from './data/raritiesObj.js';
 import { collectionsObj } from './data/collectionsObj.js';
 console.log(skinsArr, weaponsObj, raritiesObj, collectionsObj);
 
+const answer = skinsArr[Math.floor(Math.random() * 1358)];
+console.log(answer);
+let searchArr = [];
+
 const form = document.getElementById('form');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -11,5 +15,26 @@ form.addEventListener('submit', function(event) {
 
 const inputGuess = document.getElementById('inputGuess');
 inputGuess.addEventListener('input', function() {
-  console.log();
+  const inputVal = inputGuess.value.trim();
+  searchArr = [];
+
+  if(inputVal) {
+    skinsArr.forEach(element => {
+      if(element.name.toLowerCase().includes(inputVal.toLowerCase())) {
+        searchArr.push(element);
+      }
+    });
+  }
+  console.log(inputVal, searchArr);
+
+  const divRes = document.getElementById('results');
+  divRes.innerHTML = '';
+  searchArr.forEach(element => {
+    const result = document.createElement('div');
+    result.className = 'result';
+    result.style.backgroundColor = raritiesObj[element.rarity];
+    result.innerText = element.name;
+    result.onclick = '';
+    divRes.appendChild(result);
+  });
 });
